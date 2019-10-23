1ad939b884ce4194704b2b4c47263b65219f09c4
Albert Hsu <iverson52000@gmail.com>
2019-10-22 20:48:40 -0700

    20191022

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://mysterious-waters-21668.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
